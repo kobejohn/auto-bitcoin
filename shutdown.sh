72865319efb20874e5a3fa228f0efa7b80e03f10
John Nieri <john@ifrec.osaka-u.ac.jp>
2017-03-26 22:42:39 +0900

    bugfix: add explicit reference to data directory

--- shutdown.sh
+++ shutdown.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 echo "Received shutdown / pre-emption signal. Shutting down gracefully."
-if bitcoin-cli stop; then
+if bitcoin-cli -datadir=/mnt/disks/blockchain stop; then
     echo "Stopped bitcoind."
 else
     echo "Failed to stop bitcoind."
