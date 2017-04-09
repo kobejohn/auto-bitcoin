9d384046c0b4345ec249a09463189e8b1f5fecdc
John Nieri <niericentral+git@gmail.com>
2017-04-09 20:51:52 +0900

    Make cron jobs scripts so easier to set path etc.

--- startup-bootstrap.sh
+++ startup-bootstrap.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 set -e
 
+PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 AUTOBITCOIN_REPO=git://github.com/kobejohn/auto-bitcoin.git
 AUTOBITCOIN_DIR="/auto-bitcoin"
-BTC_USER=btc_user
 
 
 # base repository
@@ -13,17 +13,9 @@ if pushd "${AUTOBITCOIN_DIR}"; then
 else
     git clone ${AUTOBITCOIN_REPO} "${AUTOBITCOIN_DIR}"
 fi
-chmod +x ${AUTOBITCOIN_DIR}/maintain-system.sh
-chmod +x ${AUTOBITCOIN_DIR}/maintain-bitcoin.sh
-chmod +x ${AUTOBITCOIN_DIR}/startup-bootstrap.sh
-chmod +x ${AUTOBITCOIN_DIR}/shutdown.sh
+chmod +x ${AUTOBITCOIN_DIR}/*.sh
 
 
-# maintain system as root every 30 minutes
-echo "0,30 * * * * root ${AUTOBITCOIN_DIR}/maintain-system.sh >> /tmp/btc_maintain-system.log 2>&1" > /etc/cron.d/maintain-system
-chmod +x /etc/cron.d/maintain-system
-
-
-# maintain bitcoin as non-root every 30 minutes, "after" system maintenance
-echo "5, 35 * * * * ${BTC_USER} ${AUTOBITCOIN_DIR}/maintain-bitcoin.sh >> /tmp/btc_maintain-bitcoin.log 2>&1" > /etc/cron.d/maintain-bitcoin
-chmod +x /etc/cron.d/maintain-bitcoin
+# maintenance cron jobs
+cp cron-maintain* /etc/cron.d/
+chmod +x /etc/cron.d/cron-maintain*  # probably not necessary
