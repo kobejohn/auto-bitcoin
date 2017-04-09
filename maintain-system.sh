cc271552f08d001cc2aa288cc87af46ba3cc72f0
John Nieri <niericentral+git@gmail.com>
2017-04-09 22:31:48 +0900

    set project explicitly for cron jobs

--- maintain-system.sh
+++ maintain-system.sh
@@ -6,9 +6,10 @@ PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 AUTOBITCOIN_DIR=/auto-bitcoin
 BTC_USER=btc_user
 
+PROJECT="bu-test-160812"
 ZONE="asia-northeast1-a"
 INSTANCE_GROUP="bitcoin-instance-group"
-INSTANCE=$(gcloud compute instance-groups managed list-instances ${INSTANCE_GROUP} --zone ${ZONE} | tail -n 1 | awk '{print $1;}')
+INSTANCE=$(gcloud compute --project ${PROJECT} instance-groups managed list-instances ${INSTANCE_GROUP} --zone ${ZONE} | tail -n 1 | awk '{print $1;}')
 DISK_NAME="blockchain"
 DISK_SIZE_GB="200"
 DEVICE_DIR="/dev/disk/by-id/google-${DISK_NAME}"
@@ -26,7 +27,7 @@ fi
 
 
 echo "*****************************************\nConfirm or create blockchain disk:"
-if gcloud compute disks create ${DISK_NAME} --size "${DISK_SIZE_GB}" --type "pd-standard" --zone ${ZONE}; then
+if gcloud compute --project ${PROJECT} disks create ${DISK_NAME} --size "${DISK_SIZE_GB}" --type "pd-standard" --zone ${ZONE}; then
     echo "Created blockchain disk."
 else
     echo "Unable to create blockchain disk. Assuming it has already been created."
@@ -34,9 +35,9 @@ fi
 
 
 echo "*****************************************\nConfirm or attach blockchain disk to instance:"
-if gcloud compute instances attach-disk ${INSTANCE} --disk ${DISK_NAME} --device-name ${DISK_NAME} --mode rw --zone ${ZONE}; then
+if gcloud compute --project ${PROJECT} instances attach-disk ${INSTANCE} --disk ${DISK_NAME} --device-name ${DISK_NAME} --mode rw --zone ${ZONE}; then
     # setting no-auto-delete seems to be idemopotent-friendly (no error if already set) so just run it
-    gcloud compute instances set-disk-auto-delete ${INSTANCE} --no-auto-delete --disk ${DISK_NAME} --zone ${ZONE}
+    gcloud compute --project ${PROJECT} instances set-disk-auto-delete ${INSTANCE} --no-auto-delete --disk ${DISK_NAME} --zone ${ZONE}
     echo "Attached blockchain disk to instance."
 else
     echo "Unable to attach blockchain disk. Assuming it has already been attached."
