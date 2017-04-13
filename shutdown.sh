b1c37fcb76fede2bec12fbf54a31a6a2ba7e583b
John Nieri <niericentral+git@gmail.com>
2017-04-13 21:50:47 +0900

    bugfix? run shutdown as btcuser in case it is causing strange permissions problem on blockchain files.

--- shutdown.sh
+++ shutdown.sh
@@ -7,8 +7,10 @@ MOUNT_DIR="/mnt/disks/blockchain"
 
 
 echo "Received shutdown / pre-emption signal. Shutting down gracefully."
+su btcuser <<EOF
 if bitcoin-cli -datadir="${MOUNT_DIR}" stop; then
     echo "Stopped bitcoind."
 else
     echo "Failed to stop bitcoind."
 fi
+EOF
